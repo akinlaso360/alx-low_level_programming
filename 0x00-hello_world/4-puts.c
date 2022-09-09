a0b0d6d7ae6e0c06cd436d61c6b6d3c63c639dca
akinlaso360 <akinlasonasirudeen@gmail.com>
2022-09-08 17:17:06 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Retun: Always O (Success)
+ */
+int main(void)
+	pusts("\"Programming is like building a multilingual puzzle");
+	return (O);
+	}
