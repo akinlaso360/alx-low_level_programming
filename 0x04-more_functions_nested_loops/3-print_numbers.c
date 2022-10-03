a71e56825a22bde79dacb0055d1bc80dce815645
akinlaso360 <akinlasonasirudeen@gmail.com>
2022-10-03 02:09:11 -0700

    Task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,41 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
- */
-void print_numbers(void)
-{
-	int ch;
+* print_remaining_days - takes a date and prints how many days are
+*		left in the year, taking leap years into account
+*
+* @month: month in number format
+* @day: day of month
+* @year: year
+*/
 
-	for (ch = 48; ch < 58; ch++)
+void print_remaining_days(int month, int day, int year)
+{
+	/**
+	 * leap year when it can be divided
+	 * by 100 and 400 or by 4 evenly
+	*/
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-		_putchar(ch);
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
+
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+
 	}
-	_putchar('\n');
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
