686261d3aaa27a8466f384919b1c5b77e3b0e83f
akinlaso360 <akinlasonasirudeen@gmail.com>
2022-10-07 07:45:57 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,48 @@
 #include "main.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory.
- * @str: string.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
- * Return: pointer of an array of chars
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strout;
-	unsigned int i, j;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-	if (str == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-	if (strout == NULL)
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
 
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
 
-	return (strout);
+	sout[i] = '\0';
+
+	return (sout);
 }
